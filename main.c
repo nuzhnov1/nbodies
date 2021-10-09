e5f09329467f7944cc7417caf2ec42621eac6e1d
Andrey <andrey_nuzhnov.01@mail.ru>
2021-10-09 14:34:04 +0700

    Added check to initializing system

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #define NB_INT_SIZE 16
 
 
+#include <errno.h>
+
 #include "nb_menu.h"
 
 
@@ -10,5 +12,12 @@ int main()
     nb_system system;
 
     nb_system_init_default(&system);
+    if (errno == ENOMEM)
+    {
+        printf("Critical error: failed to initializing system.\n");
+        return -1;
+    }
     nb_menu_loop(&system);
+
+    return 0;
 }
