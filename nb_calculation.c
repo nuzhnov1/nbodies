5ab0c8a58c25bae6372cb0affe0f4442a8226211
Andrey <andrey_nuzhnov.01@mail.ru>
2021-10-09 16:36:12 +0700

    The code of the multithreaded implementation has been rewritten

--- nb_calculation.c
+++ nb_calculation.c
@@ -224,7 +224,6 @@ void nb_euler_multithreading(nb_system *const system, nb_float dt)
     const size_t offset = sizeof(nb_body); // offset to pointers
     nb_body* bodies = system->bodies;      // vector of bodies
     size_t count = system->count;          // count of bodies
-    nb_float time = system->time;          // current time
 
     // if there are no bodies in the system, then we do nothing    
     if (count == 0)
@@ -239,6 +238,10 @@ void nb_euler_multithreading(nb_system *const system, nb_float dt)
     nb_float *const fy = &bodies[0].force.y;   // "y" component of force
     nb_float *const mass = &bodies[0].mass;    // mass
     nb_float *const rad = &bodies[0].radius;   // radius
+
+    // values of speed after probably collisions
+    nb_float* const sx_new = (nb_float*)system->_calc_buf;
+    nb_float* const sy_new = (nb_float*)system->_calc_buf + count;
 
     // components of "i" body
     nb_float* cx_i;
@@ -265,8 +268,8 @@ void nb_euler_multithreading(nb_system *const system, nb_float dt)
                                  rad_i, cx_j, cy_j, sx_j, sy_j, fx_j, fy_j, \
                                  mass_j, rad_j) \
                          shared(count, offset, gravity_const, dt, cx, cy, \
-                                sx, sy, fx, fy, mass, rad) \
-                         if (count > max_threads * 10)
+                                sx, sy, fx, fy, mass, rad, sx_new, sy_new) \
+                         if (count > max_threads)
     {
         const size_t threads_count = (size_t)omp_get_num_threads();
 
@@ -276,35 +279,25 @@ void nb_euler_multithreading(nb_system *const system, nb_float dt)
         {
             fx_i = move_ptr(fx, offset * i);
             fy_i = move_ptr(fy, offset * i);
-            
+
             *fx_i = 0;
-            *fy_i = 0;
+            *fy_i = 0; 
 
             // TODO: remove this aftet debugging
             #ifdef NB_CALCULATION_DEBUG
             if (i == 0)
             {
-                printf("Was the force initialization block parallelized: "
+                printf("Was the first \"for\" block parallelized: "
                     "%s.\n", omp_in_parallel() ? "true" : "false");
             }
             #endif
         }
-        
-        // Calculate forces for all bodies in system
+
+        // Calculate forces for all bodies in system 
+        // and check probably collisions
         #pragma omp for schedule(static, count / threads_count)
-        // Calculate parameters for all bodies in system
         for (size_t i = 0; i < count; i++)
         {
-            // total mass of other bodies, which colided with body "i"
-            nb_float t_mass = 0.0;
-            // total impulse of other bodies, which colided with body "j"
-            nb_float t_impulse_x = 0.0;
-            nb_float t_impulse_y = 0.0;
-            
-            // did the body "i" collide with anyone body
-            bool is_collided = false;
-
-            // Initialize body "i" parameters
             cx_i = move_ptr(cx, offset * i);
             cy_i = move_ptr(cy, offset * i);
             sx_i = move_ptr(sx, offset * i);
@@ -314,7 +307,15 @@ void nb_euler_multithreading(nb_system *const system, nb_float dt)
             mass_i = move_ptr(mass, offset * i);
             rad_i = move_ptr(rad, offset * i);
 
-            // Initialize body "j" parameters
+            // total mass of other bodies, which colided with body "i"
+            nb_float t_mass = 0.0;
+            // total impulse of other bodies, which colided with body "j"
+            nb_float t_impulse_x = 0.0;
+            nb_float t_impulse_y = 0.0;
+            
+            // did the body "i" collide with anyone body
+            bool is_collided = false;
+
             cx_j = cx; 
             cy_j = cy;
             sx_j = sx;
@@ -324,8 +325,8 @@ void nb_euler_multithreading(nb_system *const system, nb_float dt)
             mass_j = mass;
             rad_j = rad;
 
-            // Calculate forces between body "i" and all bodies "j",
-            // when j != i. Controlling collisions
+            // Calculate forces between body "i" and all bodies "j", 
+            // when j != i
             for (size_t j = 0; j < count; j++)
             {
                 if (j == i)
@@ -381,21 +382,53 @@ void nb_euler_multithreading(nb_system *const system, nb_float dt)
                 rad_j = move_ptr(rad_j, offset);
             }
 
-            // Values of speed at current time moment
-            nb_float prev_sx_i = *sx_i;
-            nb_float prev_sy_i = *sy_i;
-
             // Calculate speed for body "i" after collisions
             if (is_collided)
             {
                 nb_float scal1 = ((*mass_i) - t_mass) / ((*mass_i) + t_mass);
                 nb_float scal2 = 2.0 / ((*mass_i) + t_mass);
 
-                *sx_i = scal1 * prev_sx_i + scal2 * t_impulse_x;
-                *sy_i = scal1 * prev_sy_i + scal2 * t_impulse_y;
+                sx_new[i] = scal1 * (*sx_i) + scal2 * t_impulse_x;
+                sy_new[i] = scal1 * (*sy_i) + scal2 * t_impulse_y;
+            }
+            // Or record current speed values if there no collisions
+            else
+            {
+                sx_new[i] = (*sx_i);
+                sy_new[i] = (*sy_i);
             }
 
-            // Calculate speed for body "i" through time "dt"
+            // TODO: remove this aftet debugging
+            #ifdef NB_CALCULATION_DEBUG
+            if (i == 0)
+            {
+                printf("Was the second \"for\" block parallelized: %s.\n", \
+                    omp_in_parallel() ? "true" : "false");
+            }
+            #endif
+        }
+
+        // Calculate new speed and new coordinates for all bodies
+        #pragma omp for schedule(static, count / threads_count)
+        for (size_t i = 0; i < count; i++)
+        {
+            cx_i = move_ptr(cx, offset * i);
+            cy_i = move_ptr(cy, offset * i);
+            sx_i = move_ptr(sx, offset * i);
+            sy_i = move_ptr(sy, offset * i);
+            fx_i = move_ptr(fx, offset * i);
+            fy_i = move_ptr(fy, offset * i);
+            mass_i = move_ptr(mass, offset * i);
+
+            // Set new speed after probably collision  
+            *sx_i = sx_new[i];
+            *sy_i = sy_new[i];
+
+            // Values of speed at current time moment
+            nb_float prev_sx_i = *sx_i;
+            nb_float prev_sy_i = *sy_i;
+
+            // Calculate new speed for body "i" through time "dt"
             *sx_i += dt * (*fx_i) / (*mass_i);
             *sy_i += dt * (*fy_i) / (*mass_i);
 
@@ -403,25 +436,16 @@ void nb_euler_multithreading(nb_system *const system, nb_float dt)
             *cx_i += dt * prev_sx_i + ((*sx_i) - prev_sx_i) * dt / 2;
             *cy_i += dt * prev_sy_i + ((*sy_i) - prev_sy_i) * dt / 2;
 
-            cx_i = move_ptr(cx_i, offset);
-            cy_i = move_ptr(cy_i, offset);
-            sx_i = move_ptr(sx_i, offset);
-            sy_i = move_ptr(sy_i, offset);
-            fx_i = move_ptr(fx_i, offset);
-            fy_i = move_ptr(fy_i, offset);
-            mass_i = move_ptr(mass_i, offset);
-            rad_i = move_ptr(rad_i, offset);
-
             // TODO: remove this aftet debugging
             #ifdef NB_CALCULATION_DEBUG
             if (i == 0)
             {
-                printf("Was the force calculation block parallelized: %s.\n", \
+                printf("Was the third \"for\" block parallelized: %s.\n", \
                     omp_in_parallel() ? "true" : "false");
             }
             #endif
         }
     }
-
-    time += dt;
+    
+    system->time += dt;
 }
--- nb_calculation.h
+++ nb_calculation.h
@@ -2,7 +2,7 @@
 #define NB_CALCULATION_H
 
 
-#define NB_CALCULATION_DEBUG
+#undef NB_CALCULATION_DEBUG
 
 
 #include "nb_system.h"
